f3021df2e7afb9f94f3c8c4f34cc59489a990b69
Igor Kastyrkin <tremen@yandex.ru>
2023-08-02 19:18:02 +0300

    initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    std::string text = "Hello, world!";
+    std::unordered_map<char, int> frequency; // создаем карту для подсчета частоты символов
+
+    // проходим по каждому символу текста
+    for (char letter : text)
+    {
+            // увеличиваем счетчик для символа, если его частота меньше текущей
+            frequency[letter]++; // увеличиваем cчеткик символов
+    }
+
+    std::vector<std::pair<char, int>> elems(frequency.begin(), frequency.end());
+
+    auto compareBySecond = [](const pair<char, int>& lhs, const pair<char, int>& rhs) {
+        return lhs.second > rhs.second; // сравниваем элементы по второму индексу
+    };
+
+    // сортируем
+    std::sort(elems.begin(), elems.end(), compareBySecond);
+
+    // печатаем
+    std::cout << "[IN]:" << text << std::endl;
+    std::cout << "[OUT]:" << std::endl;
+    for (auto& i : elems)
+    {
+        std::cout << i.first << ": " << i.second << std::endl;
+    }
+    return 0; // завершаем программу
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <map>
+
+
+int main() {
+    setlocale(0, "");
+    std::map <int, int, std::greater<int>> mp;
+
+    std::cout << "Введите количество элементов: "; int n; std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a; std::cin >> a;
+        mp[a]; // сохраняем
+    }
+
+    std::map <int, int> ::iterator it = mp.begin();
+    std::cout << "Отсортированно: " << std::endl;
+    for (; it != mp.end(); it++) {  
+        std::cout << it->first << std::endl;
+    }
+    return 0;
+}
